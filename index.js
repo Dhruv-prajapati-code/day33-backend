82fa3bfc7015585e8158641de277cfd2593b1150
Dhruv Prajapati <dhruvprajapati1622004@gmail.com>
2024-10-04 08:08:25 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const app=express();
+
+const port=8080;
+
+app.get("/request",(req,res)=>{
+    let {user,password}=req.query;
+
+    res.send(`Welcome ${user} your password is safe with us`);
+});
+
+app.post("/request",(req,res)=>{
+    let {user,password}=req.query;
+
+    res.send(`Welcome ${user,password} your password is safe with us`);//it wont work bcs post dosent sent data in query string
+});
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port} `);
+});
